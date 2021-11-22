2a6315fc81ed062e611f11d94cb725fb5319678e
Jacken <mboy@hotmail.co.uk>
2021-11-22 15:33:56 +0000

    adds cors

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
-const express = require('express');
-const { notARoute, handlesServerErrors, handlesPSQLErrors, handlesCustomErrors } = require('./controllers/errors.controllers');
-const apiRouter = require('./routes/api-router');
-
+const express = require("express");
+const {
+  notARoute,
+  handlesServerErrors,
+  handlesPSQLErrors,
+  handlesCustomErrors,
+} = require("./controllers/errors.controllers");
+const apiRouter = require("./routes/api-router");
+const cors = require("cors");
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
-app.use('/api', apiRouter);
-app.all('*', notARoute);
+app.use("/api", apiRouter);
+app.all("*", notARoute);
 
 app.use(handlesPSQLErrors);
 app.use(handlesCustomErrors);
